3318ca8e2830766eba1877db69f5c3d6e76f1ff1
Rudikov <ext.erudikov@tinkoff.ru>
2022-10-04 10:51:57 +0300

    Initial commit

---
+++ src/test/kotlin/ru/rudikov/monopoly/MonopolyApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.rudikov.monopoly
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MonopolyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
